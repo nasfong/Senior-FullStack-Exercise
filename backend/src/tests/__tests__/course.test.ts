201abe0f0c18d298b4a27ccf002ff80f75df3a8e
Samnang Tangfong <samnang.tangfong@DESTANGFONG.sabay.office>
2025-04-08 12:32:38 +0700

    fix test

--- backend/src/tests/__tests__/course.test.ts
+++ backend/src/tests/__tests__/course.test.ts
@@ -4,8 +4,8 @@ import * as courseService from '../../services/courseService';
 describe('Course', () => {
   beforeEach(async () => {
     // Clear the database and create new test courses before each test
-    await Course.create({ name: 'Course 1', description: 'Description 1', price: 100, capacity: 30, });
-    await Course.create({ name: 'Course 2', description: 'Description 2', price: 200, capacity: 30, });
+    await Course.create({ name: 'Frontend Course', description: 'Description 1', price: 100, capacity: 30, });
+    await Course.create({ name: 'Backend Course', description: 'Description 2', price: 200, capacity: 30, });
   });
 
   describe('Get List Courses', () => {
@@ -15,20 +15,20 @@ describe('Course', () => {
 
       // Check that the result matches the expected courses
       expect(result).toHaveLength(2);
-      expect(result[0].name).toBe('Course 1');
-      expect(result[1].name).toBe('Course 2');
+      expect(result[0].name).toBe('Frontend Course');
+      expect(result[1].name).toBe('Backend Course');
     });
   });
 
   describe('Search Courses', () => {
     it('should return a list of courses', async () => {
       // Call the actual service method to retrieve all courses
-      const q = 'Course 1'
+      const q = 'Frontend'
       const result = await courseService.findAll(q);
 
       // Check that the result matches the expected courses
       expect(result).toHaveLength(1);
-      expect(result[0].name).toBe('Course 1');
+      expect(result[0].name).toBe('Frontend Course');
     });
   });
 });
